4585224faf8fc1d21f2d5f26044fe70422f81ad8
Thamognya Kodi <contact@thamognya.com>
2022-07-10 20:21:14 +0700

    have to work on css

--- components/sortingVisualizer/index.tsx
+++ components/sortingVisualizer/index.tsx
@@ -1,21 +1,74 @@
 import React from 'react'
 import {useState} from 'react'
 
-class SortingVisualizer extends React.Component {
+interface ArrayState {
+    array: any;
+    arraySize: number;
+    min: number;
+    max: number;
+}
+
+export class SortingVisualizer extends React.Component<{}, ArrayState> {
+
     constructor(props: {} | Readonly<{}>) {
         super(props);
 
         this.state = {
             array: [],
+            arraySize: 300,
+            min: 5,
+            max: 1000,
         }
     }
 
+    componentDidMount() {
+        this.resetArray();
+    }
 
+    resetArray() {
+
+        const array = [];
+        const n: number = this.state.arraySize;
+        const min: number = this.state.min;
+        const max: number = this.state.max;
+
+        for (let i = 0; i < n; i++) {
+            array.push(randomIntFromInterval(min, max))
+        }
+
+        this.setState({array})
+    }
     render () {
+
+        const array = this.state.array;
+
         return (
-            <h1>hello</h1>
+        <>
+            <div className="grid gap-10 grid-rows-2">
+                <div className="flex gap-2">
+                    <h1>hello</h1>
+                    <h1>hello</h1>
+                </div>
+                <div className="absolute">
+                    {array && array.map((value: any, idx: any) => {
+                        return (
+                            <div 
+                                key={idx} 
+                                className="inline-block bg-slate-400 px-0.5 rounded-lg"
+                                style={{
+                                    height: `${value * 0.095}vh`,
+                                }}
+                            >
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </>
         )
     }
 }
 
-export default SortingVisualizer
+function randomIntFromInterval(min: any, max: any) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
