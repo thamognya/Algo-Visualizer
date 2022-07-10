ffca8ff65f2ef1b829e4bdd48393f7c2271b9c50
Thamognya Kodi <contact@thamognya.com>
2022-07-10 21:48:27 +0700

    js to ts

--- components/sortingVisualizer/index.tsx
+++ components/sortingVisualizer/index.tsx
@@ -1,74 +1,72 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 
 interface ArrayState {
-    array: any;
-    arraySize: number;
-    min: number;
-    max: number;
+    array: any
+    arraySize: number
+    min: number
+    max: number
 }
 
 export class SortingVisualizer extends React.Component<{}, ArrayState> {
-
     constructor(props: {} | Readonly<{}>) {
-        super(props);
+        super(props)
 
         this.state = {
             array: [],
             arraySize: 300,
             min: 5,
-            max: 1000,
+            max: 1000
         }
     }
 
     componentDidMount() {
-        this.resetArray();
+        this.resetArray()
     }
 
     resetArray() {
-
-        const array = [];
-        const n: number = this.state.arraySize;
-        const min: number = this.state.min;
-        const max: number = this.state.max;
+        const array = []
+        const n: number = this.state.arraySize
+        const min: number = this.state.min
+        const max: number = this.state.max
 
         for (let i = 0; i < n; i++) {
             array.push(randomIntFromInterval(min, max))
         }
 
-        this.setState({array})
+        this.setState({ array })
     }
-    render () {
-
-        const array = this.state.array;
+    render() {
+        const array = this.state.array
 
         return (
-        <>
-            <div className="grid gap-10 grid-rows-2">
-                <div className="flex gap-2">
-                    <h1>hello</h1>
-                    <h1>hello</h1>
+            <>
+                <div className="grid gap-10 grid-rows-2">
+                    <div className="flex gap-2">
+                        <h1>hello</h1>
+                        <h1>hello</h1>
+                    </div>
+                    <div className="absolute">
+                        {array &&
+                            array.map((value: any, idx: any) => {
+                                return (
+                                    <div
+                                        key={idx}
+                                        className="inline-block bg-slate-400 px-0.5 rounded-lg"
+                                        style={{
+                                            height: `${value * 0.095}vh`
+                                        }}
+                                    ></div>
+                                )
+                            })}
+                    </div>
                 </div>
-                <div className="absolute">
-                    {array && array.map((value: any, idx: any) => {
-                        return (
-                            <div 
-                                key={idx} 
-                                className="inline-block bg-slate-400 px-0.5 rounded-lg"
-                                style={{
-                                    height: `${value * 0.095}vh`,
-                                }}
-                            >
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        </>
+            </>
         )
     }
 }
 
-function randomIntFromInterval(min: any, max: any) { // min and max included 
+function randomIntFromInterval(min: any, max: any) {
+    // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 // file imports
-import {SortingVisualizer} from '../components/sortingVisualizer'
+import { SortingVisualizer } from '../components/sortingVisualizer'
 
 const Home: NextPage = () => {
     return (
