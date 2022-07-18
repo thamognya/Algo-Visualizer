39fef01a70ea3579552fb9280fb0e3423ccaced6
Thamognya Kodi <contact@thamognya.com>
2022-07-18 08:40:07 +0700

    Updates

--- _next/static/tawCFx32DGj9KVNcmyaRH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-172c3a1944a45a13.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
