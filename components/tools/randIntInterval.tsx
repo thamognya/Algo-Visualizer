17826d6d9b86d7dc39dfd55a1d5ebdd74438e2e9
Thamognya Kodi <contact@thamognya.com>
2022-07-21 19:00:49 +0700

    start over

--- components/tools/randIntInterval.tsx
+++
@@ -1,6 +0,0 @@
-const randomIntFromInterval = (min: any, max: any) => {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-export default randomIntFromInterval
